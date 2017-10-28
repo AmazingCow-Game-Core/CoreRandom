864ba73bd6fee2f4623c493ecd786a790cc19b78
n2omatt <n2omatt@amazingcow.com>
2017-10-28 10:25:31 -0200

    - Cosmetic changes.

--- include/Random.h
+++ include/Random.h
@@ -31,8 +31,9 @@ class Random
 {
     // Enums / Constants / Typedefs //
 public:
-    ///@brief A meta value to indicate to Random()
-    ///that it should use a random seed.
+    ///@brief
+    ///  A meta value to indicate to Random()
+    ///  that it should use a random seed.
     ///@see Random(), getSeed(), isUsingRandomSeed().
     static const int kRandomSeed;
 
@@ -44,84 +45,120 @@ public:
 
     // CTOR/DTOR //
 public:
-    ///@brief Constructs a Random object with the target seed.
-    ///@param seed - The seed that the random number generator will
-    ///be initialized. If seed is kRandomSeed it will be
-    ///selected from the system std::time(nullptr).
+    ///@brief
+    ///  Constructs a Random object with the target seed.
+    ///@param seed
+    ///  The seed that the random number generator will
+    ///  be initialized. If seed is kRandomSeed it will be
+    ///  selected from the system std::time(nullptr).
     ///@see kRandomSeed, reseed(), getSeed(), isUsingRandomSeed().
     Random(int seed = kRandomSeed);
 
 
     // Generation Methods //
 public:
-    ///@brief Generate a random integer between [0, std::numeric_limits<int>::max()].
-    ///@returns An integer in [0, std::numeric_limits<int>::max()] range.
-    ///@note next return is INCLUSIVE.
+    ///@brief
+    ///  Generate a random integer between [0, std::numeric_limits<int>::max()].
+    ///@returns
+    ///  An integer in [0, std::numeric_limits<int>::max()] range.
+    ///@note
+    ///  next return is INCLUSIVE.
     ///@see next(int max), next(int min, int max);
     int next();
 
-    ///@brief Generate a random integer between [0, max].
-    ///@param max - The upperbound of the range.
-    ///@warning max must be >= 0.
-    ///@returns An integer in [0, max] range.
-    ///@throws std::invalid_argument if max < 0.
-    ///@note next return is INCLUSIVE.
+    ///@brief
+    ///  Generate a random integer between [0, max].
+    ///@param max
+    ///  The upperbound of the range.
+    ///@warning
+    ///  max must be >= 0.
+    ///@returns
+    ///  An integer in [0, max] range.
+    ///@throws
+    ///  std::invalid_argument if max < 0.
+    ///@note
+    ///  next return is INCLUSIVE.
     ///@see next(), next(int min, int max);
     int next(int max);
 
-    ///@brief Generate a random integer between [min, max].
-    ///@param min - The lowerbound of the range.
-    ///@param max - The upperbound of the range.
-    ///@warning max >= min.
-    ///@returns An integer in [min, max] range.
-    ///@throws std::invalid_argument if max < min.
-    ///@note next return is INCLUSIVE.
+    ///@brief
+    ///  Generate a random integer between [min, max].
+    ///@param min
+    ///  The lowerbound of the range.
+    ///@param max
+    ///  The upperbound of the range.
+    ///@warning
+    ///  max >= min.
+    ///@returns
+    ///  An integer in [min, max] range.
+    ///@throws
+    ///  std::invalid_argument if max < min.
+    ///@note
+    ///  next return is INCLUSIVE.
     ///@see next(), next(int max);
     int next(int min, int max);
 
 
-    ///@brief Generate a random bool value.
-    ///@returns A bool value.
+    ///@brief
+    ///  Generate a random bool value.
+    ///@returns
+    ///  A bool value.
     ///@see next(), next(int max), next(int min, int max);
     bool nextBool();
 
 
     // Helper Methods //
 public:
-    ///@brief Gets the reference for the internal number generator.
-    ///@returns A reference for the internal number generator.
+    ///@brief
+    ///  Gets the reference for the internal number generator.
+    ///@returns
+    ///  A reference for the internal number generator.
     ///@see NumberGeneratorType.
     NumberGeneratorType& getNumberGenerator();
-    ///@brief Gets the constant reference for the internal number generator.
-    ///@returns A constant reference for the internal number generator.
+
+    ///@brief
+    ///  Gets the constant reference for the internal number generator.
+    ///@returns
+    ///  A constant reference for the internal number generator.
     ///@see NumberGeneratorType.
     const NumberGeneratorType& getNumberGenerator() const;
 
-    ///@brief Gets the reference for the internal integer distribution.
-    ///@returns A reference for the internal integer distribution.
+    ///@brief
+    ///  Gets the reference for the internal integer distribution.
+    ///@returns
+    ///  A reference for the internal integer distribution.
     ///@see IntegerDistributionType.
     IntegerDistributionType& getIntDistribution();
-    ///@brief Gets the constant reference for the internal integer distribution.
-    ///@returns A constant reference for the internal integer distribution.
+
+    ///@brief
+    ///  Gets the constant reference for the internal integer distribution.
+    ///@returns
+    ///  A constant reference for the internal integer distribution.
     ///@see IntegerDistributionType.
     const IntegerDistributionType& getIntDistribution() const;
 
 
-    ///@brief Reseed object with the target seed.
-    ///@param seed - The seed that the random number generator will
-    ///be initialized. If seed is kRandomSeed it will be
-    ///selected from the system std::time(nullptr).
+    ///@brief
+    ///  Reseed object with the target seed.
+    ///@param seed
+    ///  The seed that the random number generator will
+    ///  be initialized. If seed is kRandomSeed it will be
+    ///  selected from the system std::time(nullptr).
     ///@see getSeed(), isUsingRandomSeed().
     void reseed(int seed = kRandomSeed);
 
-    ///@brief Gets the actual seed used.
-    ///@returns The actual seed used in this object - If Random was created
-    ///using kRandomSeed it will the chosen seed, otherwise will return the
-    ///argument used in the CTOR.
+    ///@brief
+    ///  Gets the actual seed used.
+    ///@returns
+    ///  The actual seed used in this object - If Random was created
+    ///  using kRandomSeed it will the chosen seed, otherwise will
+    ///  return the argument used in the CTOR.
     int getSeed() const;
 
-    ///@brief Checks if CTOR was initialized with kRandomSeed.
-    ///@returns True if kRandomSeed was used, False otherwise.
+    ///@brief
+    ///  Checks if CTOR was initialized with kRandomSeed.
+    ///@returns
+    ///  True if kRandomSeed was used, False otherwise.
     bool isUsingRandomSeed() const;
 
 
@@ -141,6 +178,5 @@ private:
     IntegerDistributionType m_dist;
     IntegerDistributionType m_randomDist;
 };
-
 
 NS_CORERANDOM_END
