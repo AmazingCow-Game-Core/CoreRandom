47c6d92fdad0237d2eb17b59d4681e49b5d0cc10
n2omatt <n2omatt@amazingcow.com>
2017-10-28 10:21:07 -0200

    - Update the license headers.

--- update_version.sh
+++ update_version.sh
@@ -1,52 +1,28 @@
 #!/bin/bash
-##----------------------------------------------------------------------------##
-##               █      █                                                     ##
-##               ████████                                                     ##
-##             ██        ██                                                   ##
-##            ███  █  █  ███        update_version.sh                         ##
-##            █ █        █ █        CoreRandom                                ##
-##             ████████████                                                   ##
-##           █              █       Copyright (c) 2016                        ##
-##          █     █    █     █      AmazingCow - www.AmazingCow.com           ##
-##          █     █    █     █                                                ##
-##           █              █       N2OMatt - n2omatt@amazingcow.com          ##
-##             ████████████         www.amazingcow.com/n2omatt                ##
+##~---------------------------------------------------------------------------##
+##                     _______  _______  _______  _     _                     ##
+##                    |   _   ||       ||       || | _ | |                    ##
+##                    |  |_|  ||       ||   _   || || || |                    ##
+##                    |       ||       ||  | |  ||       |                    ##
+##                    |       ||      _||  |_|  ||       |                    ##
+##                    |   _   ||     |_ |       ||   _   |                    ##
+##                    |__| |__||_______||_______||__| |__|                    ##
+##                             www.amazingcow.com                             ##
+##  File      : update_version.sh                                             ##
+##  Project   : CoreRandom                                                    ##
+##  Date      : May 27, 2016                                                  ##
+##  License   : GPLv3                                                         ##
+##  Author    : n2omatt <n2omatt@amazingcow.com>                              ##
+##  Copyright : AmazingCow - 2016, 2017                                       ##
 ##                                                                            ##
-##                  This software is licensed as GPLv3                        ##
-##                 CHECK THE COPYING FILE TO MORE DETAILS                     ##
-##                                                                            ##
-##    Permission is granted to anyone to use this software for any purpose,   ##
-##   including commercial applications, and to alter it and redistribute it   ##
-##               freely, subject to the following restrictions:               ##
-##                                                                            ##
-##     0. You **CANNOT** change the type of the license.                      ##
-##     1. The origin of this software must not be misrepresented;             ##
-##        you must not claim that you wrote the original software.            ##
-##     2. If you use this software in a product, an acknowledgment in the     ##
-##        product IS HIGHLY APPRECIATED, both in source and binary forms.     ##
-##        (See opensource.AmazingCow.com/acknowledgment.html for details).    ##
-##        If you will not acknowledge, just send us a email. We'll be         ##
-##        *VERY* happy to see our work being used by other people. :)         ##
-##        The email is: acknowledgment_opensource@AmazingCow.com              ##
-##     3. Altered source versions must be plainly marked as such,             ##
-##        and must not be misrepresented as being the original software.      ##
-##     4. This notice may not be removed or altered from any source           ##
-##        distribution.                                                       ##
-##     5. Most important, you must have fun. ;)                               ##
-##                                                                            ##
-##      Visit opensource.amazingcow.com for more open-source projects.        ##
-##                                                                            ##
-##                                  Enjoy :)                                  ##
-##----------------------------------------------------------------------------##
-
-################################################################################
+##  Description :                                                             ##
 ## This will update the ProjectName (HEADER_FILE) and the                     ##
 ## Doxyfile (DOXY_FILE) version strings.                                      ##
 ## Very dirty, but does the job - We were forgeting to update the version     ##
 ## string before each release.                                                ##
-## Today it is local to this project (but if it become too complex) it will   ##
-## be placed into a separate project                                          ##
-################################################################################
+## Today it is local to this project (but if it become too complex) it        ##
+## will be placed into a separate project.                                    ##
+##---------------------------------------------------------------------------~##
 
 ################################################################################
 ## CONFIG                                                                     ##
