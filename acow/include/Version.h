dce5fb9aca4d89444e6dc06bb4631de3cafc4608
n2omatt <n2omatt@amazingcow.com>
2018-04-23 12:09:45 -0700

    - Change the filename and the #defines.

--- acow/include/Version.h
+++ acow/include/Version.h
@@ -20,27 +20,14 @@
 #pragma once
 
 //----------------------------------------------------------------------------//
-// Namespaces                                                                 //
-//----------------------------------------------------------------------------//
-// All classes of this core is placed inside this namespace.
-// We use MACROS so is easier to change if needed.
-// Is (in our opinion) more explicit.
-// And finally the editors will not reformat the code.
-#define NS_CORERANDOM_BEGIN namespace CoreRandom {
-#define NS_CORERANDOM_END   }
-#define USING_NS_CORERANDOM using namespace CoreRandom
-
-//----------------------------------------------------------------------------//
 // Version                                                                    //
 //----------------------------------------------------------------------------//
 // The core version number.
-#define COW_CORERANDOM_VERSION_MAJOR    "0"
-#define COW_CORERANDOM_VERSION_MINOR    "5"
-#define COW_CORERANDOM_VERSION_REVISION "0"
+#define ACOW_RANDOM_VERSION_MAJOR    "0"
+#define ACOW_RANDOM_VERSION_MINOR    "5"
+#define ACOW_RANDOM_VERSION_REVISION "0"
 
-#define COW_CORERANDOM_VERSION       \
-    COW_CORERANDOM_VERSION_MAJOR "." \
-    COW_CORERANDOM_VERSION_MINOR "." \
-    COW_CORERANDOM_VERSION_REVISION
-
-
+#define ACOW_RANDOM_VERSION           \
+        ACOW_RANDOM_VERSION_MAJOR "." \
+        ACOW_RANDOM_VERSION_MINOR "." \
+        ACOW_RANDOM_VERSION_REVISION
